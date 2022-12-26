42fcb883800b784c56be93e2c3ec7df9b0ff9c89
Vitya Shataev <vityashataev@gmail.com>
2022-12-26 11:10:22 +0300

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import Components from "unplugin-vue-components/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cost-control/",
+  base: "/cost_control_client_vue/",
   plugins: [
     vue2(),
     legacy({
